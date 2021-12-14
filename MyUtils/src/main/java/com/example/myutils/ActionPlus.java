8436c53f9bd3e3e21d60319dc1a84229a5cccc19
asifali369 <asifaliishaq.gmail.com>
2021-12-14 13:27:59 +0500

    Initial commit

---
+++ MyUtils/src/main/java/com/example/myutils/ActionPlus.java
@@ -0,0 +1,52 @@
+package com.example.myutils;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class ActionPlus extends AppCompatActivity {
+    private Button btn_result;
+    private EditText edt_first_number, edt_second_number;
+    int first_number, second_number;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_action_plus);
+        BindViews();
+    }
+
+    private void BindViews() {
+        btn_result = findViewById(R.id.btn_result);
+        edt_first_number = findViewById(R.id.edt_first_number);
+        edt_second_number = findViewById(R.id.edt_second_number);
+    }
+
+    private void initClickListener() {
+        btn_result.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edt_first_number.getText().toString().trim().isEmpty()) {
+                    edt_first_number.setError("Enter First Number");
+                }
+                if (edt_second_number.getText().toString().trim().isEmpty()) {
+                    edt_second_number.setError("Enter Second Number");
+                }
+                if (TextUtils.isDigitsOnly(edt_first_number.getText()) &&
+                        TextUtils.isDigitsOnly(edt_second_number.getText())) {
+                    try {
+                        first_number = Integer.parseInt(edt_first_number.getText().toString().trim());
+                        second_number = Integer.parseInt(edt_second_number.getText().toString().trim());
+                        MyMath.addition(first_number, second_number);
+                    } catch (NumberFormatException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+}
--- MyUtils/src/main/java/com/example/myutils/MyMath.java
+++ MyUtils/src/main/java/com/example/myutils/MyMath.java
@@ -1,7 +1,17 @@
 package com.example.myutils;
 
+import android.content.Context;
+import android.content.Intent;
+
 public class MyMath {
     public static int addition(int first_number, int second_number){
         return first_number+second_number;
     }
+
+    public static void start_activity(Context context){
+        {
+            context.startActivity(new Intent(context,ActionPlus.class));
+        }
+    }
+
 }
